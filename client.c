1d7747b50b506b20b7daac8122491525b701d9b5
mehan <mehan.alavi.88.majd@gmail.com>
2025-01-30 17:13:01 +0330

    init

---
+++ client.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    // Create socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Set up server address
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Connect to the server
+    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    printf("Connected to server!\n");
+
+    // Receive data from the server
+    char buf[1024] = {0};
+    recv(sockfd, buf, sizeof(buf), 0);
+    printf("Received from server: %s\n", buf);
+
+    // Close the socket
+    close(sockfd);
+    printf("Client socket closed.\n");
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    // Create socket
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    int opt = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
+        perror("setsockopt failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    // Set up server address
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(8080);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Bind to localhost only
+
+    // Bind socket to address
+    bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+
+    // Listen for incoming connections
+    listen(sockfd, 10);
+    printf("Server listening on port 8080...\n");
+
+    // Accept a new connection
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen = sizeof(client_addr);
+    int client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addrlen);
+    printf("Client connected!\n");
+    printf(client_addr.sin_addr.s_addr);
+
+    // Send data to the client
+    char buf[] = "hello tcp!";
+    send(client_sockfd, buf, sizeof(buf), 0);
+    printf("Message sent to client: %s\n", buf);
+
+
+    // Close the client socket
+    close(client_sockfd);
+    printf("Client socket closed.\n");
+
+    // Close the server socket
+    close(sockfd);
+    printf("Server soket closed.\n");
+
+    return 0;
+}
